762b811120ec6368ffb942cc12185527b9ae2c2a
Dias <dias.atudinov@gmail.com>
2024-12-25 14:37:40 +0500

    Managers add

--- Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Managers/DeviceInfo.swift
+++ Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Managers/DeviceInfo.swift
@@ -1,3 +1,11 @@
+//
+//  DeviceInfo.swift
+//  Leprechaun Rainbow Crafter
+//
+//  Created by Dias Atudinov on 25.12.2024.
+//
+
+
 import UIKit
 
 class DeviceInfo {
--- Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Managers/User.swift
+++ Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Managers/User.swift
@@ -1,3 +1,11 @@
+//
+//  User.swift
+//  Leprechaun Rainbow Crafter
+//
+//  Created by Dias Atudinov on 25.12.2024.
+//
+
+
 import SwiftUI
 
 class User: ObservableObject {
