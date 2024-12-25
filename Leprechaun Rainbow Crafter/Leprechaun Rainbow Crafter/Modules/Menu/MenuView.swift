2a2ff74a8c68ebe32598f4dde8b172a2ae0c7760
Dias <dias.atudinov@gmail.com>
2024-12-25 14:37:54 +0500

    Menu and splash screen add

--- Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Modules/Menu/MenuView.swift
+++ Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Modules/Menu/MenuView.swift
@@ -8,8 +8,200 @@
 import SwiftUI
 
 struct MenuView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showCollections = false
+    @State private var showGame = false
+    @State private var showShop = false
+    @State private var showRules = false
+    @State private var showSettings = false
+    
+    
+    @StateObject var user = User.shared
+      @StateObject var settingsVM = SettingsModel()
+//    @StateObject var shopVM = ShopViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Spacer()
+                
+                if geometry.size.width < geometry.size.height {
+                    // Вертикальная ориентация
+                    ZStack {
+                        
+                        VStack {
+                            HStack(spacing: 5){
+                                Spacer()
+                              
+                                ZStack {
+                                    Image(.coinBg)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: DeviceInfo.shared.deviceType == .pad ? 160:86)
+                                    TextWithBorder(text: "\(user.coins * 100)", font: .custom(Fonts.regular.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 80 :30), textColor: .appWhite, borderColor: .appYellow, borderWidth: 1)
+                                        .textCase(.uppercase)
+                                        .padding(.leading, 48)
+                                }.frame(height: DeviceInfo.shared.deviceType == .pad ? 100:55)
+                                Spacer()
+                            }
+                            Spacer()
+                        }.padding()
+                        
+                        HStack {
+                            Spacer()
+                            VStack(spacing: 25) {
+                                
+                                
+                                Button {
+                                    showCollections = true
+                                } label: {
+                                    TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Collections", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                
+                                Button {
+                                    showShop = true
+                                } label: {
+                                    TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Shop", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                Button {
+                                    showRules = true
+                                } label: {
+                                    TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "How to play", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                Button {
+                                    showSettings = true
+                                } label: {
+                                    TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Settings", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                                Button {
+                                    showGame = true
+                                } label: {
+                                    TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Start", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                }
+                                
+                            }
+                            Spacer()
+                        }
+                    }
+                } else {
+                    ZStack {
+                        
+                        VStack {
+                            HStack(spacing: 5){
+                                Spacer()
+                                ZStack {
+                                    Image(.coinBg)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: DeviceInfo.shared.deviceType == .pad ? 160:86)
+                                    TextWithBorder(text: "\(user.coins * 100)", font: .custom(Fonts.regular.rawValue, size: DeviceInfo.shared.deviceType == .pad ? 80 :30), textColor: .appWhite, borderColor: .appYellow, borderWidth: 1)
+                                        .textCase(.uppercase)
+                                        .padding(.leading, 48)
+                                }.frame(height: DeviceInfo.shared.deviceType == .pad ? 100:55)
+                                Spacer()
+                            }.padding([.top, .trailing], 20)
+                            Spacer()
+                        }
+                        
+                        VStack {
+                            Spacer()
+                            
+                            VStack(spacing: 25) {
+                                Spacer()
+                                
+                                HStack(spacing: 25) {
+                                    Spacer()
+                                    Button {
+                                        
+                                        showCollections = true
+                                    } label: {
+                                        TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Collections", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    
+                                    
+                                    Button {
+                                        
+                                        showShop = true
+                                    } label: {
+                                        TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Shop", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    Spacer()
+                                }
+                                
+                                HStack(spacing: 25) {
+                                    Spacer()
+                                    Button {
+                                        showRules = true
+                                    } label: {
+                                        TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "How to play", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    
+                                    Button {
+                                        showSettings = true
+                                    } label: {
+                                        TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "Settings", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    Spacer()
+                                }
+                                
+                                HStack(spacing: 25) {
+                                    Spacer()
+                                    Button {
+                                        showGame = true
+                                    } label: {
+                                        TextBg(height: DeviceInfo.shared.deviceType == .pad ? 100 : 50, text: "START", textSize: DeviceInfo.shared.deviceType == .pad ? 40 : 24)
+                                    }
+                                    Spacer()
+                                }
+                            }.padding(.bottom, 32)
+                           
+                        }
+                        
+                        
+                    }
+                }
+            }
+            .background(
+                Image(.appBg)
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+                    .scaledToFill()
+                
+            )
+//            .onAppear {
+//                if settingsVM.musicEnabled {
+//                    MusicPlayer.shared.playBackgroundMusic()
+//                }
+//            }
+//            .onChange(of: settingsVM.musicEnabled) { enabled in
+//                if enabled {
+//                    MusicPlayer.shared.playBackgroundMusic()
+//                } else {
+//                    MusicPlayer.shared.stopBackgroundMusic()
+//                }
+//            }
+            .fullScreenCover(isPresented: $showCollections) {
+               // TeamView(viewModel: teamVM)
+            }
+            .fullScreenCover(isPresented: $showGame) {
+               // GameView(teamVM: teamVM)
+            }
+            .fullScreenCover(isPresented: $showShop) {
+                //ShopView(viewModel: shopVM)
+            }
+            .fullScreenCover(isPresented: $showRules) {
+                //RulesView()
+            }
+            .fullScreenCover(isPresented: $showSettings) {
+                SettingsView(settings: settingsVM)
+                
+            }
+            
+        }
     }
 }
 
--- Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Modules/Menu/SplashScreen.swift
+++ Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Modules/Menu/SplashScreen.swift
@@ -8,8 +8,82 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var progress: CGFloat = 0.0
+    @State private var timer: Timer?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    
+                    Spacer()
+                    
+                }
+                Spacer()
+                
+                
+            }
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    // Grayscale image
+                    Image("logoLRC")
+                        .resizable()
+                        .scaledToFit()
+                        .colorMultiply(.gray)
+                    
+                    // Color overlay with mask
+                    Image("logoLRC")
+                        .resizable()
+                        .scaledToFit()
+                        .mask(
+                            Rectangle()
+                                .frame(height: progress * 200) // Adjust height based on progress
+                                .padding(.bottom, (1 - progress) * 200) // Align at the bottom
+                        )
+                }
+                            .frame(height: 200)
+                TextWithBorder(text: "LOADING...", font: .custom(Fonts.regular.rawValue, size: 50), textColor: .appWhite, borderColor: .yellow, borderWidth: 1)
+
+                    .frame(height: 51)
+                    .scaleEffect(scale)
+                    .animation(
+                        Animation.easeInOut(duration: 0.8)
+                            .repeatForever(autoreverses: true),
+                        value: scale
+                    )
+                    .onAppear {
+                        scale = 0.8
+                    }
+                    .padding(.bottom, 15)
+            }
+        }.background(
+            Image(.appBg)
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+            
+        )
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        timer?.invalidate()
+        progress = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.07, repeats: true) { timer in
+            if progress < 1 {
+                progress += 0.01
+            } else {
+                timer.invalidate()
+            }
+        }
     }
 }
 
