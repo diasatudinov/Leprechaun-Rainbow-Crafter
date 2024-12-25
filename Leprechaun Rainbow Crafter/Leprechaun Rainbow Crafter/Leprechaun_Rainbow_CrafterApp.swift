83787954393be6ca82c217dc2855d3b45f87fba6
Dias <dias.atudinov@gmail.com>
2024-12-25 12:38:16 +0500

    first commit

---
+++ Leprechaun Rainbow Crafter/Leprechaun Rainbow Crafter/Leprechaun_Rainbow_CrafterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leprechaun_Rainbow_CrafterApp.swift
+//  Leprechaun Rainbow Crafter
+//
+//  Created by Dias Atudinov on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Leprechaun_Rainbow_CrafterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
